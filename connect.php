23756437d7caf218545a5b5c3f0d8705bc760af1
Dushyant Rathore <dushyant.bgs@gmail.com>
2016-10-22 11:58:54 +0530

    Database Connection File

---
+++ connect.php
@@ -0,0 +1,33 @@
+<?php
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+
+$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+
+/*
+if ($conn)
+{
+	echo "Connected Successfully";
+}
+else
+{
+	echo mysql_error();
+}
+*/
+
+$conn_db = mysql_select_db("ATM_System"); #Select a Database after Connection
+
+/*
+if($conn_db)
+{
+	echo "<br>"."Connected to Database Successfully" ;
+}
+else
+{
+	echo "<br>"."Could not connect Successfully" ;
+}
+*/
+
+?>
