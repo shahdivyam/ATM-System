265498babb2f057c54d09b4848662f07e8d5ca8b
shahdivyam <divyamshah15@gmail.com>
2016-10-24 23:24:59 +0530

    updated succesful.php

--- successful.php
+++ successful.php
@@ -8,8 +8,12 @@ $database = "ATM_System";
 // Create connection
 $conn = new mysqli($servername, $username, $password,$database);
 
-$checkQuery = "SELECT * FROM Customers WHERE AccountNo = $account_no";
-$query = "INSERT INTO Customers VALUES ($account_no,$pin_no,$contact_no,$balance )";
+global $checkQuery;
+$checkQuery = "SELECT AccountNo FROM Customers WHERE AccountNo = $account_no";
+
+global $query;
+
+$query = "INSERT INTO Customers VALUES ('$account_no','$pin_no','$contact_no','$balance')";
 
 $account_no = $_POST["account_no"];
 $pin_no = $_POST["pin_no"];
@@ -71,32 +75,48 @@ function check(){
 		$re_pin_no_err="";
 	}
 	global $conn;
+	global $account_no;
+	$checkQuery = "SELECT AccountNo FROM Customers WHERE AccountNo = '$account_no'";
 	$result = $conn->query($checkQuery);
-	if($result->num_rows!=0){
+	$count = mysqli_num_rows($result);
+	if($count != 0 ){
+		
 		global $user_exists;
 		$user_exists = 1;
+		echo "user Exists value  : $user_exists";
 	}
+	
+	echo "user Exists value  : $user_exists";
 	global $all_fine;
 	global $user_exists;
+	
 	if($all_fine!=0&&$user_exists==0){
+		
+		global $pins_match;global $account_no;global $pin_no;global $contact_no ;global $balance;
 		global $conn;
-		$conn->query($query);
-		 echo "New record created successfully <br><br>";
-    global $pins_match;global  $account_no;global  $pin_no;global  $re_pin_no;global  $contact_no ;global $balance;
+		global $query;
+		$query = "INSERT INTO Customers VALUES ('$account_no','$pin_no','$contact_no','$balance')";
+		$conn->query($query) ;		
+			echo "New record created successfully <br><br>";
+	    
+			echo "Account Number : $account_no <br> Pin Number : $pin_no<br> Contact Number : $contact_no<br> Balance :$balance <br>" ;
+			echo "Error: " . $sql . "<br>" . $conn->error;
 		
-    echo "Account Number : $account_no <br> Pin Number : $pin_no<br> Contact Number : $contact_no<br> Balance :$balance <br>" ;
-		}
+	}
+		
 	else if($all_fine==1){
-		echo " Account number already Exists , please Login ";
-		
-		}
+		echo " Account number already Exists , please Login ";	
+		echo "Error: " . "<br>" . $conn->error;
+			
+	}
+	
 	else{
 		echo "Error: " . $sql . "<br>" . $conn->error;
 		global $pins_match_err;global  $account_no_err;global  $pin_no_err;global  $re_pin_no_err;global  $contact_no_err ;global $balance_err;
 		echo " <br> $pins_match_err $account_no_err $pin_no_err $re_pin_no_err $contact_no_err $balance_err " ;
 
-		}
 	}
+}
 // Main function call , for executing registering new user  !
 
 check();
