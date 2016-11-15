13eb4c6377bdfeda5ea45442383cad141c90c1d8
Dushyant Rathore <dushyant.bgs@gmail.com>
2016-11-16 01:19:18 +0530

    Update history.php

--- history.php
+++ history.php
@@ -11,41 +11,42 @@ $query_run = mysql_query($sql) ;
 
 echo 
 "
-<h1 align = 'center'><u>Previous Transactions</u></h1>
-<br>
-<br>
-<table border = '2px' width = '700px' align = 'center'>
-<tr>
-<th>Date</th>
-<th>Amount</th>
-<th>Type</th>
-</tr>
-" ;
-
-while($row = mysql_fetch_assoc($query_run))
-{
-	$account = $row['Account_No'] ;
-	$date = $row['Time'] ;
-	$amount = $row['Amount'] ;
-	$type = $row['Type'] ; 
-
-	if ($type == '0')
-	{
-		echo 
-		"<tr>
-		<td>$date</td>
-		<td>$amount</td>
-		<td style = 'background-color:red ; color : white ;'>Debit</td>
+<body bgcolor='#2F4F4F' style='color:white'>
+	<h1 align = 'center' style = 'color:white; font-size:100px;'><u>Previous Transactions</u></h1>
+	<br>
+	<br>
+	<table border = '5px' width = '700px' align = 'center' style='border-color:white;border-collapse: collapse;'>
+		<tr>
+			<th>Date</th>
+			<th>Amount</th>
+			<th>Type</th>
 		</tr>
 		" ;
-	}
-	else if ($type == '1')
-	{
-		echo 
-		"<tr>
-		<td>$date</td>
-		<td>$amount</td>
-		<td style = 'background-color:green ; color : white ;'>Credit</td>
+
+		while($row = mysql_fetch_assoc($query_run))
+		{
+			$account = $row['Account_No'] ;
+			$date = $row['Time'] ;
+			$amount = $row['Amount'] ;
+			$type = $row['Type'] ; 
+
+			if ($type == '0')
+			{
+				echo 
+				"<tr>
+				<td>$date</td>
+				<td>$amount</td>
+				<td style = 'background-color:red ; color : white ;'>Debit</td>
+			</tr>
+			" ;
+		}
+		else if ($type == '1')
+		{
+			echo 
+			"<tr>
+			<td>$date</td>
+			<td>$amount</td>
+			<td style = 'background-color:green ; color : white ;'>Credit</td>
 		</tr>
 		" ;
 	}
